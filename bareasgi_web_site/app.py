c1a0de7d8c95caa0ca8dfc44fcdad62a3c78a81a
Rob Blackbourn <rob.blackbourn@googlemail.com>
2019-11-10 19:19:40 +0000

    Added why

--- bareasgi_web_site/app.py
+++ bareasgi_web_site/app.py
@@ -10,7 +10,7 @@ import jinja2
 import pkg_resources
 
 from .markdown_handler import get_markdown
-from .front_page import get_frontpage
+from .front_page import get_frontpage, get_why
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,7 +19,7 @@ def create_application(
         config: Dict[str, Any],
         assets_path: str
 ) -> Application:
-    """Create the aplication"""
+    """Create the application"""
     info = {
         'config': config,
         'assets_path': assets_path
@@ -27,6 +27,7 @@ def create_application(
     app = Application(info=info)
 
     app.http_router.add({'GET'}, '/', get_frontpage)
+    app.http_router.add({'GET'}, '/why', get_why)
     app.http_router.add({'GET'}, '/tutorial/{docs:path}', get_markdown)
 
     add_static_file_provider(
--- bareasgi_web_site/front_page.py
+++ bareasgi_web_site/front_page.py
@@ -25,3 +25,16 @@ async def get_frontpage(
     return {
         'title': 'bareASGI'
     }
+
+
+@bareasgi_jinja2.template('why.html')
+async def get_why(
+        _scope: Scope,
+        _info: Info,
+        _matches: RouteMatches,
+        _content: Content
+) -> Dict[str, Any]:
+    """A request handler"""
+    return {
+        'title': 'bareASGI'
+    }
