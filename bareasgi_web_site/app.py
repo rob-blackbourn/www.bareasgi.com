bf6af4e1dc6b2e58d62f7cbf4bca8b290668accd
Rob Blackbourn <rob.blackbourn@gmail.com>
2019-11-10 12:52:43 +0000

    Added jinja2

--- bareasgi_web_site/app.py
+++ bareasgi_web_site/app.py
@@ -5,6 +5,9 @@ from typing import Any, Dict
 
 from bareasgi import Application
 from bareasgi_static import add_static_file_provider
+import bareasgi_jinja2
+import jinja2
+import pkg_resources
 
 from .markdown_handler import get_markdown
 from .front_page import get_frontpage
@@ -31,5 +34,13 @@ def create_application(
         source_folder=assets_path,
         mount_point='/assets/'
     )
+
+    templates = pkg_resources.resource_filename(__name__, "templates")
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(templates),
+        # autoescape=jinja2.select_autoescape(['html', 'xml']),
+        enable_async=True
+    )
+    bareasgi_jinja2.add_jinja2(app, env)
 
     return app
--- bareasgi_web_site/markdown_handler.py
+++ bareasgi_web_site/markdown_handler.py
@@ -2,17 +2,16 @@
 
 import os.path
 import logging
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 import aiofiles
 from bareasgi import (
     Scope,
     Info,
     RouteMatches,
-    Content,
-    HttpResponse,
-    text_writer
+    Content
 )
+import bareasgi_jinja2
 from markdown import markdown
 
 LOGGER = logging.getLogger(__name__)
@@ -33,49 +32,29 @@ async def _get_document(
         return await file_ptr.read()
 
 
+@bareasgi_jinja2.template('markdown.html')
 async def get_markdown(
         _scope: Scope,
         info: Info,
         matches: RouteMatches,
         _content: Content
-) -> HttpResponse:
+) -> Dict[str, Any]:
     """A request handler"""
-    try:
-        config = info['config']
-        doc_folders = [os.path.expandvars(path) for path in config['docs']]
-        path = matches['docs']
-        text = await _get_document(path, doc_folders)
-        if not path.endswith('.md'):
-            text = f"""
+    config = info['config']
+    doc_folders = [os.path.expandvars(path) for path in config['docs']]
+    path = matches['docs']
+    text = await _get_document(path, doc_folders)
+    if not path.endswith('.md'):
+        text = f"""
 # {path}
 
 ```
 {text}
 ```
 """
-        md_text = markdown(text, extensions=['extra', 'codehilite'])
-        md_html = markdown(md_text)
-
-        html = f"""
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>bareASGI</title>
-    <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon">    
-    <link href="//cdn.muicss.com/mui-0.10.0/css/mui.min.css" rel="stylesheet" type="text/css" />
-    <script src="//cdn.muicss.com/mui-0.10.0/js/mui.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/codehilite.css">
-  </head>
-  <body>
-    <div class="mui-container-fluid">
-      <div class="mui-panel">
-        {md_html}
-      </div>
-    </div>
-  </body>
-</html>
-"""
-        return 200, [(b'content-type', b'text/html')], text_writer(html)
-    except Exception as error:  # pylint: disable=bare-except
-        return 500
+    md_text = markdown(text, extensions=['extra', 'codehilite'])
+    md_html = markdown(md_text)
+    return {
+        'title': 'bareASGI tutorial',
+        'md_html': md_html
+    }
